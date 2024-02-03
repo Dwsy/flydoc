e12d37fd77058470d769de446b49fd30d43ba8dc
Dwsy <dwsycode@gmail.com>
2024-02-03 20:58:57 +0800

    e..

--- .vitepress/enhanceApp.ts
+++ .vitepress/enhanceApp.ts
@@ -1,6 +1,6 @@
-import { defineClientAppEnhance } from '@vuepress/client'
-import ElementPlus from 'element-plus'
-import 'element-plus/dist/index.css'
-export default defineClientAppEnhance(({ app, router, siteData }) => {
-    app.use(ElementPlus)
-})
+// import { defineClientAppEnhance } from '@vuepress/client'
+// import ElementPlus from 'element-plus'
+// import 'element-plus/dist/index.css'
+// export default defineClientAppEnhance(({ app, router, siteData }) => {
+//     app.use(ElementPlus)
+// })
